a677ceb5b5278a099dcd51b5d1028532fe6c5e85
Sudqi Jawabreh <s.jawabreh@foothillsolutions.com>
2021-09-20 19:50:27 +0300

    step 1,2 get sum of numbers separated by commas

--- StringCalculatorKata/Program.cs
+++ StringCalculatorKata/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Linq;
 
 namespace StringCalculatorKata
 {
     class Program
     {
+        public static void AssertTrue(bool condition)
+        {
+            if (!condition)
+                throw new Exception();
+        }
+        public static int Add(string input)
+        {
+            var sum = input.Split(",").Select(n => int.Parse(n)).Sum();
+            return sum;
+        }
         static void Main(string[] args)
         {
+            AssertTrue(Add("1,2") == 3);
+            AssertTrue(Add("1,23") == 24);
             Console.WriteLine("Hello World!");
         }
     }
