1074e77bf243299fe5c4e892bd494e91a71a5501
Sudqi Jawabreh <s.jawabreh@foothillsolutions.com>
2021-09-20 20:02:18 +0300

    Handel \n as delimiter

--- StringCalculatorKata/Program.cs
+++ StringCalculatorKata/Program.cs
@@ -10,15 +10,34 @@ namespace StringCalculatorKata
             if (!condition)
                 throw new Exception();
         }
+        public static void AssertThrowException(Action action)
+        {
+            var thrown = false;
+            try
+            {
+                action();
+            }
+            catch(Exception ex)
+            {
+                thrown = true;
+            }
+            if (!thrown)
+            {
+                throw new Exception();
+            }
+        }
         public static int Add(string input)
         {
-            var sum = input.Split(",").Select(n => int.Parse(n)).Sum();
+            var sum = input.Split(',','\n').Select(n => int.Parse(n)).Sum();
             return sum;
         }
         static void Main(string[] args)
         {
             AssertTrue(Add("1,2") == 3);
             AssertTrue(Add("1,23") == 24);
+
+            AssertTrue(Add("10\n20,30") == 60);
+            AssertThrowException(() => Add("1,\n23"));
             Console.WriteLine("Hello World!");
         }
     }
