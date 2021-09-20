d0cf35bfecad585c5d2d7a8eb7dd4fa3e3f04027
Sudqi Jawabreh <s.jawabreh@foothillsolutions.com>
2021-09-20 20:23:35 +0300

    don't support negative numbers

--- StringCalculatorKata/Program.cs
+++ StringCalculatorKata/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StringCalculatorKata
@@ -26,20 +27,48 @@ namespace StringCalculatorKata
                 throw new Exception();
             }
         }
+        public static void AssertThrowExceptionWithMessage(Action action, string message)
+        {
+            
+            var thrown = false;
+            try
+            {
+                action();
+            }
+            catch(Exception ex)
+            {
+                if(ex.Message == message)
+                    thrown = true;
+            }
+            if (!thrown)
+            {
+                throw new Exception();
+            }
+        }
         public static int Add(string input)
         {
             var delimiter = ',';
             var sum = 0;
+            var numberStrings = new List<string>(); 
             if (input.StartsWith("//"))
             {
                 var firstLine = input.Substring(2).TakeWhile(ch => ch != '\n');
                 delimiter = firstLine.First();
-                sum = input.Substring(2 + firstLine.Count() + 1).Split(delimiter, '\n').Select(n => int.Parse(n)).Sum();
+                numberStrings = input.Substring(2 + firstLine.Count() + 1).Split(delimiter, '\n').ToList();
             }
             else
             {
-                sum = input.Split(delimiter, '\n').Select(n => int.Parse(n)).Sum();
+                numberStrings = input.Split(delimiter, '\n').ToList();
             }
+
+            var numbers = numberStrings.Select(n => int.Parse(n));
+            var negatives = numbers.Where(n => n < 0);
+            var positives = numbers.Where(n => n >= 0);
+
+            if (negatives.Any())
+                throw new Exception($"negatives not allowed: {string.Join(",", negatives)}");
+
+            sum = positives.Sum();
             return sum;
         }
         static void Main(string[] args)
@@ -53,6 +82,8 @@ namespace StringCalculatorKata
             AssertTrue(Add("//;\n1;2") == 3);
             AssertTrue(Add("//;\n25;30;60") == 115);
             AssertTrue(Add("//;\n25;30\n60") == 115);
+
+            AssertThrowExceptionWithMessage(() => Add("1,-2,-23"), "negatives not allowed: -2,-23");
             //AssertTrue(Add("1,23") == 24);
             Console.WriteLine("Hello World!");
         }
